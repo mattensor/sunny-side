aaaff79152d08d743ef3e84fda45f67a3ab83030
Matt Ensor <matt.ensor@buildkite.com>
2026-06-11 11:09:40 +1000

    ci: wire up tests plugin with bktec for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,11 @@ export default defineConfig({
 	test: {
 		environment: "node",
 		include: ["src/**/*.test.ts"],
-		slowTestThreshold: 200
+		slowTestThreshold: 200,
+		reporters: ['default', 'json'],
+		outputFile: 'tmp/vitest-results.json',
+
+		// Enable column + line capture for Test Engine
+  		includeTaskLocation: true,
 	}
 })
